8ae6b8d7026e855604a22bfc36c5b694526d535b
Luis J Pacheco <89895893+fredabomb50@users.noreply.github.com>
2023-05-04 13:01:57 -0400

    ran PPM test; starting writing class for handling vector-3s
    ran PPM test; starting writing class for handling vector-3s

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
-
+//#include <iostream>
+#include <vec3.h>
 
 using std::cout;
 
@@ -14,14 +14,21 @@ int main()
 
     for ( int j = y - 1; j >= 0; j-- )
     {
-        for ( int k = 0; k < x; k++ )
+        for ( int i = 0; i < x; i++ )
         {
+            // store as 0 - 1.0 range values
             float r, g, b;
-            r = float(k) / float(x);
+            r = float(i) / float(x);
             g = float(j) / float(y);
             b = 0.3;
 
-            
+            // storing back as 0 - 255 values for logging
+            int log_R, log_G, log_B;
+            log_R = int(255.00 * r);
+            log_G = int(255.00 * g);
+            log_B = int(255.00 * b);
+
+            cout << log_R << " " << log_G << " " << log_B << "\n";
         }
     }
 }
---
+++ src/vec3.h
@@ -0,0 +1,81 @@
+#include <math.h>
+#include <iostream>
+#include <stdlib.h>
+
+using std::istream;
+using std::ostream;
+
+
+class vec3
+{
+    public:
+        float e[3];
+
+        vec3() {}
+        vec3(float e0, float e1, float e2) { e[0] = e0; e[1] = e1; e[2] = e2; }
+
+        // Points
+        inline float x() { return e[0]; }
+        inline float y() { return e[1]; }
+        inline float z() { return e[2]; }
+
+        // Colors
+        inline float r() { return e[0]; }
+        inline float g() { return e[1]; }
+        inline float b() { return e[2]; }
+
+        // return pointer of vec3
+        inline const vec3& operator+() const { return *this; }
+
+        // return vec3 with inverted values
+        inline vec3 operator-() const { return vec3( -e[0], -e[1], -e[2] ); }
+
+        // return entry at index
+        inline float operator[](int index) const { return e[index]; }
+
+        // return pointer to entry at index
+        inline float& operator[](int index) { return e[index]; }
+
+
+        // Function definitions
+        inline vec3& operator+=(const vec3& other_vec3);
+        inline vec3& operator-=(const vec3& other_vec3);
+        inline vec3& operator*=(const vec3& other_vec3);
+        inline vec3& operator/=(const vec3& other_vec3);
+        inline vec3& operator*=(const float val);
+        inline vec3& operator/=(const float val);
+        inline void make_unit_vector();
+
+
+        // Size and stats
+        inline float length() const 
+        {
+            return sqrt( e[0]*e[0] + e[1]*e[1] + e[2]*e[2] );
+        }
+
+        inline float squared_length() const
+        {
+            return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+        }
+};
+
+
+inline istream& operator>>(istream& stream, vec3& v)
+{   
+    stream >> v.e[0] >> v.e[1] >> v.e[2];
+    return stream;
+}
+
+inline ostream& operator<<(ostream& stream, vec3& v)
+{
+    stream << v.e[0] << " " << v.e[1] << " " << v.e[2];
+    return stream;
+}
+
+inline void vec3::make_unit_vector()
+{
+    float k = 1.0 / vec3::length();
+    e[0] *= k;
+    e[1] *= k;
+    e[2] *= k;
+}
