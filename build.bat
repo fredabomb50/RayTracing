1cfa1d5d8cd0dd47b6ae725df333384fbd1069c8
Luis J Pacheco <89895893+fredabomb50@users.noreply.github.com>
2023-05-10 11:02:20 -0400

    PPM is now live and build.bat is better

--- build.bat
+++ build.bat
@@ -1,5 +1,9 @@
 @echo off
 
+:: Compile and link staticly a new build executable
 cd C:\msys64\mingw64\bin\
 g++.exe -static -g C:\Users\lpacheco\Documents\GitHub\RayTracing\src\main.cpp -o C:\Users\lpacheco\Documents\GitHub\RayTracing\build\main.exe
-cmd /k
+
+:: Go to build path and run executable
+cd C:\Users\lpacheco\Documents\GitHub\RayTracing\build\
+main.exe
--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,29 @@
 using std::cout;
 
 
-// Function declarations
+// ===============================================
+//          FUNCTION DECLARATIONS
+// ===============================================
 vec3 color(const ray& r);
+bool hit_sphere( const vec3& center, float radius, const ray& r);
 
 
-// QUICK NOTE LEADING TO THE ADDITION OF A SPHERE FOR RAY TRACING AGAINST:
+// quadratic formula:
 //  x = (-b ± √(b^2 - 4ac)) / (2a)
 
 
 int main()
 {
     // declare file dimensions
-    int x = 300;
-    int y = 200;
-
-    // quick PPM test
-    // ppm_TEST( 255 );
+    int x = 200;
+    int y = 100;
 
     // declare file header
-    cout << "P3\n" << x << " " << y << "\n255\n";
+    FILE* test_file = createPPM( "test.ppm", x, y, 255 );
 
     // declare viewport
     vec3 lower_left_corner(-2.0, -1.0, -1.0);
 
-    // 
     vec3 horizontal(4.0, 0.0, 0.0);
     vec3 vertical(0.0, 2.0, 0.0);
 
@@ -51,21 +50,43 @@ int main()
             log_G = int(255.99 * color_val[1]);
             log_B = int(255.99 * color_val[2]);
 
-            cout << log_R << " " << log_G << " " << log_B << "\n";
+            addToPPM(test_file, log_R, log_G, log_B);
         }
     }
+
+    // free resources
+    fclose(test_file);
 }
 
 
-// function definitions
+// ===============================================
+//          FUNCTION DEFINITIONS
+// ===============================================
    
 /*Color lerp between 2 values; default is white and a light blue*/
 vec3 color(const ray& r)
 {
-    vec3 unit_directon = unit_vector( r.direction() );
+    // check to see if ray collides with theoretical sphere, and return that color instead
+    if ( hit_sphere(vec3(0, 0, -1), 0.5, r) ) { return vec3(1, 0, 0); }
 
+
+    vec3 unit_directon = unit_vector( r.direction() );
     float t = 0.5 * (unit_directon.y() + 1.0);
 
     // blended = (1.0 - t) * start_value + t * end_value, 
     return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
+
+
+// Generate a hypothetical sphere at center and of radius
+bool hit_sphere( const vec3& center, float radius, const ray& r)
+{
+    vec3 oc = r.origin() - center;
+    float a, b, c;
+    a = dot(r.direction(), r.direction());
+    b = 2.0 * dot(oc, r.direction());
+    c = dot(oc, oc) - radius*radius;
+    float discriminant = b*b - 4*a*c;
+
+    return (discriminant > 0);
 }
